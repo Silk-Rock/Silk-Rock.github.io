4940345e561d60a69ae3c80edae4cba3930336e3
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2022-04-04 17:22:24 -0400

    additional CSS

---
+++ resources/scripts/includer.js
@@ -0,0 +1,30 @@
+// This script allows you to include segments of HTML in multiple documents
+includeHTML();
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("html-include");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("html-include");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", "./resources/html/" + file + ".html", true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
+        }
+    }
+}
