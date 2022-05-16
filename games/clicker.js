9206eb4018a53907647a6fbebb73dff6f139170f
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2022-05-15 21:31:05 -0400

    Don't know if this will work

---
+++ games/clicker.js
@@ -0,0 +1,70 @@
+﻿var clicks = 0;
+checkCookie();
+// var messages = new Array();
+var messages;//  = fetch('./clicker.json');
+
+fetch("./clicker.json").then(response => {
+	messages = response.json();
+});
+
+renderClicks();
+
+function doClick() {
+	clicks += 1;
+	renderClicks();
+	d = new Date();
+	d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+	var expires = "expires=" + d.toUTCString();
+	document.cookie = "clicks=" + clicks + ";" + expires + ";";
+}
+
+function renderClicks() {
+	document.getElementById("button").innerHTML = "<p>" + clicks + "</p>";
+	if (messages[clicks]) {
+		document.getElementById("button").innerHTML += "<p>" + messages[clicks] + "</p>";
+	}
+	else {
+		document.getElementById("button").innerHTML += "...";
+	}
+
+	if (clicks >= 671) {
+		document.getElementById("button").style.color = "black";
+		document.getElementById("button").style.backgroundColor = "#ddd";
+	}
+	else if (clicks >= 666) {
+		document.getElementById("button").style.color = "red";
+		document.getElementById("button").style.backgroundColor = "black";
+	}
+}
+
+function setCookie(cname, cvalue, exdays) {
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	let expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for (let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return 0;
+}
+
+function checkCookie() {
+	let cookieValue = getCookie("clicks");
+	if (cookieValue != "") {
+		clicks = Number(cookieValue);
+	} else {
+		clicks = 0;
+	}
+}
